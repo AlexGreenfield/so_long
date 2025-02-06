80f87594b3a4d6bce253337b8dac229de86b28b7
Alejandro Castroverde Rodrigo <acastrov@c3r9s6.42madrid.com>
2025-02-06 19:24:17 +0100

    working on test for minilibx

--- so_long.h
+++ so_long.h
@@ -6,13 +6,31 @@
 /*   By: acastrov <acastrov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 19:42:07 by acastrov          #+#    #+#             */
-/*   Updated: 2025/02/06 17:26:47 by acastrov         ###   ########.fr       */
+/*   Updated: 2025/02/06 19:14:52 by acastrov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// User libraries
-#include "minilibx-linux/mlx.h"
-#include "minilibx-linux/mlx_int.h"
+#ifndef SO_LONG_H
+# define SO_LONG_H
 
+// User libraries
+# include "minilibx-linux/mlx.h"
+# include "minilibx-linux/mlx_int.h"
+
+// X11 libraries
+#include <X11/keysym.h>
+
+// Macros
+# define MALLOC_ERROR -1
+# define X_ERROR -2
 // System libraries
 //#include <X11/Xlib.h>
+
+// MLX struct
+typedef struct s_mlx_data
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+}	t_mlx_data;
+
+#endif
--- test.c
+++ test.c
@@ -14,15 +14,25 @@
 
 int	main(int argc, char **argv)
 {
-	void	*mlx;
-	void	*mlx_window;
-	int		i;
+	t_mlx_data	mlx;
+	int			i;
 
-	mlx = mlx_init();
-	mlx_window = mlx_new_window(mlx, 600, 600, "so_long");
+	mlx.mlx_ptr = mlx_init();
+	if (mlx.mlx_ptr == NULL)
+		return (X_ERROR);
+	mlx.win_ptr = mlx_new_window(mlx.mlx_ptr, 600, 600, "so_long");
+	if (mlx.win_ptr == NULL)
+	{
+		mlx_destroy_display(mlx.mlx_ptr);
+		free(mlx.mlx_ptr);
+		return (X_ERROR);
+	}
 	i = 0;
 	while (i++ < 100)
-		mlx_pixel_put(mlx, mlx_window, 250 + i, 250, 0x00ff00);
-	mlx_loop(mlx);
+		mlx_pixel_put(mlx.mlx_ptr, mlx.win_ptr, 250 + i, 250, 0x00ff00);
+	mlx_loop(mlx.mlx_ptr);
+	mlx_destroy_window(mlx.mlx_ptr, mlx.win_ptr);
+	mlx_destroy_display(mlx.mlx_ptr);
+	free(mlx.mlx_ptr);
 	return (0);
 }
